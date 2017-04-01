959d364b45a58bfa8c4cbcb423168b0fe0ebf586
Lucas Bertoni <lucas.bertoni@360pi.com>
2017-04-01 15:33:27 -0400

    Attempting to create the action bar

--- app/src/main/java/com/example/lucas/classbook/Schedule.java
+++ app/src/main/java/com/example/lucas/classbook/Schedule.java
@@ -2,6 +2,7 @@ package com.example.lucas.classbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Schedule extends AppCompatActivity {
 
@@ -10,4 +11,12 @@ public class Schedule extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        // Inflate the menu; this adds the action bar if it's present
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
 }
